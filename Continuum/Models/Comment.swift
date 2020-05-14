d29458941be956ad15a47a713c8ff6e470aea56e
Bryce Bradshaw <brycewbradshaw@icloud.com>
2020-05-14 14:36:22 -0600

    Comment model Finished.

--- Continuum/Models/Comment.swift
+++ Continuum/Models/Comment.swift
@@ -10,4 +10,13 @@ import Foundation
 
 class Comment {
     
+    var text: String
+    var timestamp: Date
+    weak var post: Post?
+    
+    init(text: String, timestamp: Date = Date(), post: Post?) {
+        self.text = text
+        self.timestamp = timestamp
+        self.post = post
+    }
 }
